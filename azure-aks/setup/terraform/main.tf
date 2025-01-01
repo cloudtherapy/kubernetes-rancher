b84370362a4a9b00fc201de2eebba210e6f70f14
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-31 23:39:37 -0500

    Added network_plugin_mode to overlay

--- azure-aks/setup/terraform/main.tf
+++ azure-aks/setup/terraform/main.tf
@@ -40,6 +40,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
   network_profile {
     network_plugin    = "azure"
+    network_plugin_mode = "overlay"
     load_balancer_sku = "standard"
   }
 }
