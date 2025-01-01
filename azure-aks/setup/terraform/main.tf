918f854f4fcd2eb234a36e2e645ce3d82de1ee96
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-31 23:25:02 -0500

    Change network_plugin from kubenet to azure

--- azure-aks/setup/terraform/main.tf
+++ azure-aks/setup/terraform/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     }
   }
   network_profile {
-    network_plugin    = "kubenet"
+    network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
 }
