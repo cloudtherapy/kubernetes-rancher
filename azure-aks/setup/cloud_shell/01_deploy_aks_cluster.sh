9d60b459ec2d9be9fa2527e8bdc8e1d9defa4064
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-01-01 00:13:43 -0500

    Updated cluster name

--- azure-aks/setup/cloud_shell/01_deploy_aks_cluster.sh
+++ azure-aks/setup/cloud_shell/01_deploy_aks_cluster.sh
@@ -6,11 +6,11 @@ az group create --name rg-${APP} --location ${LOCATION}
 echo "Creating AKS cluster"
 az aks create \
   --resource-group rg-${APP} \
-  --name ${APP}-aks-cluster \
+  --name cluster-aks-${APP} \
   --kubernetes-version ${K8S_VERSION} \
   --node-count ${NODE_COUNT} \
   --node-vm-size ${VM_SIZE} \
   --generate-ssh-keys
 
 echo "Getting credentials and storing them in kubeconfig"
-az aks get-credentials --resource-group rg-${APP} --name ${APP}-aks-cluster --overwrite-existing
+az aks get-credentials --resource-group rg-${APP} --name cluster-aks-${APP} --overwrite-existing
